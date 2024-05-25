72a698af54449ea75e424ec8e40883d1ed0afbe3
Eclisher <hei.harizo@gmail.com>
2024-05-26 00:15:24 +0300

    chore: add  Stock::MovementType to approvisionnement

--- src/main/java/com/kfc/sakafo/Model/Menu.java
+++ src/main/java/com/kfc/sakafo/Model/Menu.java
@@ -4,10 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class Menu {
     private int id;
     private String name;
+    private List<IngredientMenu> ingredients = new ArrayList<>();
+    // Ajouter un ingrédient au menu
+    public void addIngredient(IngredientMenu ingredientMenu) {
+        ingredients.add(ingredientMenu);
+    }
+
+    // Supprimer un ingrédient du menu
+    public void removeIngredient(Ingredient ingredient) {
+        ingredients.removeIf(ingredientMenu -> ingredientMenu.getIngredient().getId() == ingredient.getId());
+    }
+
+    // Modifier la quantité d'un ingrédient dans le menu
+    public void updateIngredientQuantity(Ingredient ingredient, double newQuantity) {
+        for (IngredientMenu ingredientMenu : ingredients) {
+            if (ingredientMenu.getIngredient().getId() == ingredient.getId()) {
+                ingredientMenu.setQuantite(newQuantity);
+                break;
+            }
+        }
+    }
 }
